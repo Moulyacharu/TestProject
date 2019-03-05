8408eefd198d66eb9a09d34af9872dfb78408bf5
Moulyacharu <moulya.gowda@gmail.com>
2019-03-05 21:55:23 +0530

    test

--- TestNgTestingProject/src/com/feb24/Class1LogicImpl.java
+++ TestNgTestingProject/src/com/feb24/Class1LogicImpl.java
@@ -7,7 +7,7 @@ public class Class1LogicImpl implements UsingInterface {
 	List<Integer> list = new ArrayList<Integer>();
 	
 	public void listTest() {
-		int arr[] = { 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0 };
+		int arr[] = { 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0,1,2 };
 		for (int i = 0; i < arr.length; i++) {
 			if (arr[i] == 0) {
 				list.add(0, arr[i]);
